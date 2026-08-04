d6338b2a5f1f23b2b29497b6b9b5241642f295de
mohamed <bnmb6301@gmail.com>
2026-08-04 08:35:50 +0300

    update build script

--- lib/Common/widgets/MainScreen.dart
+++ lib/Common/widgets/MainScreen.dart
@@ -28,10 +28,12 @@ class _BottombarState extends State<Bottombar> {
   @override
   Widget build(BuildContext context) {
     final bool isDarkMode = context.isDarkMode;
+    // استدعاء المساحة السفلية الآمنة برمجياً لضبط الارتفاع بدقة على جميع الشاشات
+    final double bottomPadding = MediaQuery.of(context).padding.bottom;
 
     return Scaffold(
       backgroundColor: isDarkMode ? const Color(0xFF121212) : Colors.white,
-      extendBody: true, // تمدد محتوى الصفحة خلف الشريط
+      extendBody: true, // ضروري لظهور العناصر خلف الشريط الشفاف
       body: PageTransitionSwitcher(
         duration: const Duration(milliseconds: 300),
         transitionBuilder: (child, animation, secondaryAnimation) {
@@ -43,117 +45,114 @@ class _BottombarState extends State<Bottombar> {
         },
         child: _pages[_currentindex],
       ),
-      bottomNavigationBar: SafeArea(
-        bottom:
-            true, // السماح للشريط بالنزول واستغلال مسافة الأمان في الآيفون بشكل صحيح
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: ClipRRect(
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(30),
-              topRight: Radius.circular(30),
-            ),
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 15.0, sigmaY: 15.0),
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-                decoration: BoxDecoration(
-                  color: isDarkMode
-                      ? const Color(0xFF181818).withOpacity(0.85)
-                      : Colors.white.withOpacity(0.85),
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
+      bottomNavigationBar: Container(
+        // حل جذري لمشكلة الفراغ: نضع Margin ديناميكي يتأقلم مع نوع الشاشة
+        margin: EdgeInsets.only(
+          left: 16,
+          right: 16,
+          bottom: bottomPadding > 0 ? bottomPadding : 16,
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(
+            30,
+          ), // حواف دائرية ناعمة من كل الاتجاهات
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 15.0, sigmaY: 15.0),
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              decoration: BoxDecoration(
+                color: isDarkMode
+                    ? const Color(0xFF181818).withOpacity(0.85)
+                    : Colors.white.withOpacity(0.85),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: BottomNavigationBar(
+                currentIndex: _currentindex,
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                type: BottomNavigationBarType.fixed,
+                enableFeedback: false,
+                showSelectedLabels: false,
+                showUnselectedLabels: false,
+                selectedItemColor: Colors.green,
+                unselectedItemColor: isDarkMode
+                    ? Colors.grey[400]
+                    : Colors.grey[600],
+                onTap: (int index) {
+                  setState(() {
+                    _currentindex = index;
+                  });
+                },
+                items: [
+                  // 1. الرئيسية
+                  BottomNavigationBarItem(
+                    icon: SvgPicture.asset(
+                      'Assest/Vectors/Home.svg',
+                      height: _currentindex == 0 ? 35 : 28,
+                      width: _currentindex == 0 ? 35 : 28,
+                      colorFilter: ColorFilter.mode(
+                        _currentindex == 0
+                            ? Colors.green
+                            : (isDarkMode
+                                  ? Colors.grey[400]!
+                                  : Colors.grey[600]!),
+                        BlendMode.srcIn,
+                      ),
+                    ),
+                    label: '',
                   ),
-                ),
-                child: BottomNavigationBar(
-                  currentIndex: _currentindex,
-                  backgroundColor: Colors.transparent,
-                  elevation: 0,
-                  type: BottomNavigationBarType.fixed,
-                  enableFeedback: false,
-                  showSelectedLabels: false,
-                  showUnselectedLabels: false,
-                  selectedItemColor: Colors.green,
-                  unselectedItemColor: isDarkMode
-                      ? Colors.grey[400]
-                      : Colors.grey[600],
-                  onTap: (int index) {
-                    setState(() {
-                      _currentindex = index;
-                    });
-                  },
-                  items: [
-                    // 1. الرئيسية
-                    BottomNavigationBarItem(
-                      icon: SvgPicture.asset(
-                        'Assest/Vectors/Home.svg',
-                        height: _currentindex == 0 ? 35 : 28,
-                        width: _currentindex == 0 ? 35 : 28,
-                        colorFilter: ColorFilter.mode(
-                          _currentindex == 0
-                              ? Colors.green
-                              : (isDarkMode
-                                    ? Colors.grey[400]!
-                                    : Colors.grey[600]!),
-                          BlendMode.srcIn,
-                        ),
+                  // 2. المكتبة
+                  BottomNavigationBarItem(
+                    icon: SvgPicture.asset(
+                      'Assest/Vectors/albumsMusic.svg',
+                      height: _currentindex == 1 ? 35 : 28,
+                      width: _currentindex == 1 ? 35 : 28,
+                      colorFilter: ColorFilter.mode(
+                        _currentindex == 1
+                            ? Colors.green
+                            : (isDarkMode
+                                  ? Colors.grey[400]!
+                                  : Colors.grey[600]!),
+                        BlendMode.srcIn,
                       ),
-                      label: '',
                     ),
-                    // 2. المكتبة
-                    BottomNavigationBarItem(
-                      icon: SvgPicture.asset(
-                        'Assest/Vectors/albumsMusic.svg',
-                        height: _currentindex == 1 ? 35 : 28,
-                        width: _currentindex == 1 ? 35 : 28,
-                        colorFilter: ColorFilter.mode(
-                          _currentindex == 1
-                              ? Colors.green
-                              : (isDarkMode
-                                    ? Colors.grey[400]!
-                                    : Colors.grey[600]!),
-                          BlendMode.srcIn,
-                        ),
+                    label: '',
+                  ),
+                  // 3. البحث
+                  BottomNavigationBarItem(
+                    icon: SvgPicture.asset(
+                      'Assest/Vectors/Search.svg',
+                      height: _currentindex == 2 ? 35 : 28,
+                      width: _currentindex == 2 ? 35 : 28,
+                      colorFilter: ColorFilter.mode(
+                        _currentindex == 2
+                            ? Colors.green
+                            : (isDarkMode
+                                  ? Colors.grey[400]!
+                                  : Colors.grey[600]!),
+                        BlendMode.srcIn,
                       ),
-                      label: '',
                     ),
-                    // 3. البحث
-                    BottomNavigationBarItem(
-                      icon: SvgPicture.asset(
-                        'Assest/Vectors/Search.svg',
-                        height: _currentindex == 2 ? 35 : 28,
-                        width: _currentindex == 2 ? 35 : 28,
-                        colorFilter: ColorFilter.mode(
-                          _currentindex == 2
-                              ? Colors.green
-                              : (isDarkMode
-                                    ? Colors.grey[400]!
-                                    : Colors.grey[600]!),
-                          BlendMode.srcIn,
-                        ),
+                    label: '',
+                  ),
+                  // 4. الملف الشخصي
+                  BottomNavigationBarItem(
+                    icon: SvgPicture.asset(
+                      'Assest/Vectors/Profile.svg',
+                      height: _currentindex == 3 ? 35 : 28,
+                      width: _currentindex == 3 ? 35 : 28,
+                      colorFilter: ColorFilter.mode(
+                        _currentindex == 3
+                            ? Colors.green
+                            : (isDarkMode
+                                  ? Colors.grey[400]!
+                                  : Colors.grey[600]!),
+                        BlendMode.srcIn,
                       ),
-                      label: '',
                     ),
-                    // 4. الملف الشخصي
-                    BottomNavigationBarItem(
-                      icon: SvgPicture.asset(
-                        'Assest/Vectors/Profile.svg',
-                        height: _currentindex == 3 ? 35 : 28,
-                        width: _currentindex == 3 ? 35 : 28,
-                        colorFilter: ColorFilter.mode(
-                          _currentindex == 3
-                              ? Colors.green
-                              : (isDarkMode
-                                    ? Colors.grey[400]!
-                                    : Colors.grey[600]!),
-                          BlendMode.srcIn,
-                        ),
-                      ),
-                      label: '',
-                    ),
-                  ],
-                ),
+                    label: '',
+                  ),
+                ],
               ),
             ),
           ),
